4d74b65bdc242507b2c76309663132a972f7c07a
Waylan <waylan49@gmail.com>
2021-04-06 16:49:44 -0500

    1st upload

---
+++ Warranty_Check/app.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(shinyMatrix)
+
+m<-matrix(c(1:6), 6, 1, dimnames = list(NULL, c("Serial Number Input")))
+
+ui <- fluidPage(
+
+    titlePanel("Warranty Check Tool"),
+
+    fluidRow(
+       
+        column(
+            4,
+            tags$h4("Data"),
+            textAreaInput(inputId = "SN", placeholder = "Input you SN#", label = "Serial Number", height = "650px"),
+            actionButton("button1","Submit!")
+        ),
+
+        
+        column(
+           8,
+           dataTableOutput("table")
+        )
+    )
+)
+
+
+server <- function(input, output){
+    observeEvent(input$button1, {
+    output$table<-renderDataTable({
+       SN_LIST<-str_split(input$SN, "\n")
+       do.call(rbind, lapply(SN_LIST[[1]][1:(length(SN_LIST[[1]])-1)], wcheck))
+    })
+    })
+    
+}
+
+
+
+shinyApp(ui = ui, server = server)
